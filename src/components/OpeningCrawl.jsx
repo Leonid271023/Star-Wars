f3f782d38a67b5ea144e0d69834454cbfe1d2ce3
leonid <lashurov44@gmail.com>
2025-07-09 00:49:00 +0300

    update OpeningCrawl.jsx

--- src/components/OpeningCrawl.jsx
+++ src/components/OpeningCrawl.jsx
@@ -1,11 +1,31 @@
-import {openingCrawl} from "../utils/components.js";
+import {useEffect, useState} from "react";
+import {base_URL, openingCrawl} from "../utils/components.js";
 
 const OpeningCrawl = () => {
-    return (
-        <p className={"farGalaxy"}>
-            {openingCrawl}
-        </p>
-    );
+    const [OpeningCrawl, setOpeningCrawl] = useState('')
+
+    useEffect(() => {
+        const episode = Math.floor(Math.random() * 6) + 1;
+        fetch(`${base_URL}/v1/films/${episode}`)
+            .then(res => res.json())
+            .then(data => setOpeningCrawl(data.opening_crawl));
+        // return () => console.log("Opening Crawl was unmounted");
+    }, [])
+    if (openingCrawl) {
+        return (
+            <p className={"farGalaxy"}>
+                {OpeningCrawl}
+            </p>
+        )
+    } else {
+        return (
+            <p className={'farGalaxy'}>
+                <span className={'spinner-border spinner-border-sm'}></span>
+                Loading...
+            </p>
+        )
+    }
+
 };
 
 export default OpeningCrawl;
