a5bc090a60c6206efce10bcd4545a69edd9a2cb4
leonid <lashurov44@gmail.com>
2025-07-08 22:35:11 +0300

    Add friend Array

--- src/components/DreamTeam.jsx
+++ src/components/DreamTeam.jsx
@@ -1,25 +1,9 @@
-import friend1 from '../images/friend1.jpg';
-import friend2 from '../images/friend2.jpg';
-import friend3 from '../images/friend3.jpg';
-import friend4 from '../images/friend4.jpg';
-import friend5 from '../images/friend5.jpg';
-import friend6 from '../images/friend6.jpg';
-import friend7 from '../images/friend7.jpg';
-import friend8 from '../images/friend8.jpg';
-import friend9 from '../images/friend9.jpg';
+import {friend} from '../utils/components.js';
 const DreamTeam = () => {
     return (
         <section className={"float-end w-50 row border rounded-bottom-4 ms-0 me-2"}>
             <h2 className={"col-12 text-center"}>Dream team</h2>
-            <img className={"col-4 p-1"} src={friend1} alt={"Friend1"}/>
-            <img className={"col-4 p-1"} src={friend2} alt={"Friend2"}/>
-            <img className={"col-4 p-1"} src={friend3} alt={"Friend3"}/>
-            <img className={"col-4 p-1"} src={friend4} alt={"Friend4"}/>
-            <img className={"col-4 p-1"} src={friend5} alt={"Friend5"}/>
-            <img className={"col-4 p-1"} src={friend6} alt={"Friend6"}/>
-            <img className={"col-4 p-1 bottomLeft"} src={friend7} alt={"Friend7"}/>
-            <img className={"col-4 p-1"} src={friend8} alt={"Friend8"}/>
-            <img className={"col-4 p-1 bottomRight"} src={friend9} alt={"Friend9"}/>
+            {friend.map((f, i) => <Friend picture={f} key={i} pos={i+1}/>)}
         </section>
     );
 };
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className={'py-5 rounded-bottom-4 row align-items-center'}>
+        <footer className={'py-5 rounded-bottom-4 row align-items-center m-0'}>
             <div className={"btn btn-danger col-sm-2 offset-2"}>
                 <p className={"m-0"}>Send me an <span className={"text-dark text-uppercase"}>email</span></p>
             </div>
