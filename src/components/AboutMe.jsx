bd95e81440b8acdf237d28424128a39526fbdc9e
leonid <lashurov44@gmail.com>
2025-07-13 18:40:25 +0300

    homework update Contact.jsx and AboutMe.jsx

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,7 +1,27 @@
+import React, {useEffect} from "react";
+import {base_URL} from "../utils/components.js";
+
 const AboutMe = () => {
-    return (
+    const[AboutMe, setAboutMe] = React.useState({});
+    useEffect(() => {
+        fetch(`${base_URL}/v1/peoples/1`)
+            .then(res => res.json())
+            .then(data => setAboutMe({
+                name: data.name,
+                gender: data.gender,
+                hair_color: data.hair_color,
+                birth_year: data.birth_year,
+            }))
+    }, []);
+
+    return(
         <div>
-            About ME
+            <h3>
+                Name: {AboutMe.name} <br/>
+                Gender: {AboutMe.gender} <br/>
+                Hair color: {AboutMe.hair_color} <br/>
+                Birth Year: {AboutMe.birth_year} <br/>
+            </h3>
         </div>
     );
 };
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,37 @@
+import {useEffect, useState} from "react";
+import {base_URL} from "../utils/components.js";
+
 const Contact = () => {
+    const [planets, setPlanets] = useState(['wait...']);
+    useEffect(() => {
+        fetch(`${base_URL}/v1/planets`)
+            .then(res => res.json())
+            .then(data => setPlanets(data.map(item=>item.name)))
+    },[])
+
+
+
     return (
-        <div>
-            Contact
-        </div>
-    );
-};
+
+            <form className={'container'} onSubmit={e => e.preventDefault()}>
+
+                <label>First Name</label>
+                <input type="text" placeholder="Your name.."/>
+
+                <label>Last Name</label>
+                <input type="text" placeholder="Your last name.."/>
+
+                <label>Planet</label>
+                <select name='planet'>
+                    {planets.map(item=><option value={item} key={item}>{item}</option>)}
+                </select>
+
+                <label>Subject
+                <textarea placeholder="Write something.."></textarea>
+                </label>
+                <button type={'submit'}>Submit</button>
+                </form>
+                );
+            };
 
 export default Contact;
