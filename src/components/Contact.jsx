96c5750b98806e384ca0f8419f3624a8b921204a
leonid <lashurov44@gmail.com>
2025-07-13 18:54:40 +0300

    useEffect with async await

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,37 +1,43 @@
+import '../Contact.css'
 import {useEffect, useState} from "react";
 import {base_URL} from "../utils/components.js";
 
 const Contact = () => {
     const [planets, setPlanets] = useState(['wait...']);
-    useEffect(() => {
-        fetch(`${base_URL}/v1/planets`)
-            .then(res => res.json())
-            .then(data => setPlanets(data.map(item=>item.name)))
-    },[])
 
+    async function getPlanets() {
+        const res = await fetch(`${base_URL}/v1/planets`);
+        const data = await res.json();
+        setPlanets(data.map(item => item.name));
+
+    }
+
+    useEffect(() => {
+        getPlanets().then(() => console.log('Planets were loaded'));
+    }, [])
 
 
     return (
 
-            <form className={'container'} onSubmit={e => e.preventDefault()}>
+        <form className='container' onSubmit={e => e.preventDefault()}>
 
-                <label>First Name</label>
-                <input type="text" placeholder="Your name.."/>
+            <label>First Name</label>
+            <input type="text" placeholder="Your name.."/>
 
-                <label>Last Name</label>
-                <input type="text" placeholder="Your last name.."/>
+            <label>Last Name</label>
+            <input type="text" placeholder="Your last name.."/>
 
-                <label>Planet</label>
-                <select name='planet'>
-                    {planets.map(item=><option value={item} key={item}>{item}</option>)}
-                </select>
+            <label>Planet</label>
+            <select name='planet'>
+                {planets.map(item => <option value={item} key={item}>{item}</option>)}
+            </select>
 
-                <label>Subject
+            <label>Subject
                 <textarea placeholder="Write something.."></textarea>
-                </label>
-                <button type={'submit'}>Submit</button>
-                </form>
-                );
-            };
+            </label>
+            <button type={'submit'}>Submit</button>
+        </form>
+    );
+};
 
 export default Contact;
